ff4e219dbb2dee61c43fbb3b90d13d037d151a55
lessw2020 <lessw@etrillium.com>
2025-07-30 20:09:37 -0700

    add support for b200

--- qutlass/csrc/gemm.cu
+++ qutlass/csrc/gemm.cu
@@ -1,5 +1,6 @@
 /*
- * Copyright (C) 2025 Roberto L. Castro (Roberto.LopezCastro@ist.ac.at). All Rights Reserved.
+ * Copyright (C) 2025 Roberto L. Castro (Roberto.LopezCastro@ist.ac.at). All
+ * Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,8 +21,8 @@
 #include <c10/cuda/CUDAGuard.h>
 
 #include "cutlass/cutlass.h"
-#include "cutlass/gemm/collective/collective_builder.hpp"
 #include "cutlass/epilogue/collective/collective_builder.hpp"
+#include "cutlass/gemm/collective/collective_builder.hpp"
 #include "cutlass/gemm/device/gemm_universal_adapter.h"
 #include "cutlass/gemm/kernel/gemm_universal.hpp"
 #include "cutlass/util/packed_stride.hpp"
@@ -30,13 +31,13 @@
 #include "cutlass/util/distribution.h"
 #include "cutlass/util/host_tensor.h"
 
-#include "cutlass/util/tensor_view_io.h"
 #include "cutlass/util/reference/device/gemm.h"
 #include "cutlass/util/reference/device/tensor_compare.h"
-#include "cutlass/util/reference/host/tensor_fill.h"
 #include "cutlass/util/reference/host/gett.hpp"
-#include "cutlass/util/reference/host/tensor_norm.h"
 #include "cutlass/util/reference/host/tensor_compare.h"
+#include "cutlass/util/reference/host/tensor_fill.h"
+#include "cutlass/util/reference/host/tensor_norm.h"
+#include "cutlass/util/tensor_view_io.h"
 
 #include "cutlass/detail/sm100_blockscaled_layout.hpp"
 
@@ -58,179 +59,171 @@ using OperatorClass = cutlass::arch::OpClassBlockScaledTensorOp;
 template <typename PerSmTileShape_MNK, typename ClusterShape, typename ArchTag>
 using CollectiveEpilogue =
     typename cutlass::epilogue::collective::CollectiveBuilder<
-        ArchTag, OperatorClass,
-        PerSmTileShape_MNK, ClusterShape,
-        cutlass::epilogue::collective::EpilogueTileAuto,
-        ElementAccumulator, ElementAccumulator,
-        ElementC, LayoutCTag, AlignmentC,
-        ElementD, LayoutDTag, AlignmentD,
-        cutlass::epilogue::collective::EpilogueScheduleAuto
-        >::CollectiveOp;
+        ArchTag, OperatorClass, PerSmTileShape_MNK, ClusterShape,
+        cutlass::epilogue::collective::EpilogueTileAuto, ElementAccumulator,
+        ElementAccumulator, ElementC, LayoutCTag, AlignmentC, ElementD,
+        LayoutDTag, AlignmentD,
+        cutlass::epilogue::collective::EpilogueScheduleAuto>::CollectiveOp;
 
-template <typename MmaTileShape, typename ClusterShape, typename PerSmTileShape_MNK,
-          typename ArchTag,
-          typename ElementA, typename LayoutATag, int AlignmentA,
-          typename ElementB, typename LayoutBTag, int AlignmentB>
+template <typename MmaTileShape, typename ClusterShape,
+          typename PerSmTileShape_MNK, typename ArchTag, typename ElementA,
+          typename LayoutATag, int AlignmentA, typename ElementB,
+          typename LayoutBTag, int AlignmentB>
 using CollectiveMainloop =
     typename cutlass::gemm::collective::CollectiveBuilder<
-        ArchTag, OperatorClass,
-        ElementA, LayoutATag, AlignmentA,
-        ElementB, LayoutBTag, AlignmentB,
-        ElementAccumulator,
-        MmaTileShape, ClusterShape,
-        cutlass::gemm::collective::StageCountAutoCarveout<
-          static_cast<int>(
-            sizeof(typename CollectiveEpilogue<PerSmTileShape_MNK,
-                                               ClusterShape,
+        ArchTag, OperatorClass, ElementA, LayoutATag, AlignmentA, ElementB,
+        LayoutBTag, AlignmentB, ElementAccumulator, MmaTileShape, ClusterShape,
+        cutlass::gemm::collective::StageCountAutoCarveout<static_cast<int>(
+            sizeof(typename CollectiveEpilogue<PerSmTileShape_MNK, ClusterShape,
                                                ArchTag>::SharedStorage))>,
-        cutlass::gemm::collective::KernelScheduleAuto
-        >::CollectiveOp;
+        cutlass::gemm::collective::KernelScheduleAuto>::CollectiveOp;
 
-template <typename MmaTileShape, typename ClusterShape, typename PerSmTileShape_MNK,
-          typename ArchTag,
-          typename ElementA, typename LayoutATag, int AlignmentA,
-          typename ElementB, typename LayoutBTag, int AlignmentB>
-using GemmKernel =
-    cutlass::gemm::kernel::GemmUniversal<
-        Shape<int, int, int, int>,
-        CollectiveMainloop<MmaTileShape, ClusterShape, PerSmTileShape_MNK,
-                           ArchTag,
-                           ElementA, LayoutATag, AlignmentA,
-                           ElementB, LayoutBTag, AlignmentB>,
-        CollectiveEpilogue<PerSmTileShape_MNK, ClusterShape, ArchTag>,
-        void>;
+template <typename MmaTileShape, typename ClusterShape,
+          typename PerSmTileShape_MNK, typename ArchTag, typename ElementA,
+          typename LayoutATag, int AlignmentA, typename ElementB,
+          typename LayoutBTag, int AlignmentB>
+using GemmKernel = cutlass::gemm::kernel::GemmUniversal<
+    Shape<int, int, int, int>,
+    CollectiveMainloop<MmaTileShape, ClusterShape, PerSmTileShape_MNK, ArchTag,
+                       ElementA, LayoutATag, AlignmentA, ElementB, LayoutBTag,
+                       AlignmentB>,
+    CollectiveEpilogue<PerSmTileShape_MNK, ClusterShape, ArchTag>, void>;
 
 // TODO(later): fix single-template issue
-template <typename MmaTileShape, typename ClusterShape, typename PerSmTileShape_MNK,
-          typename ArchTag,
-          typename ElementA, typename LayoutATag, int AlignmentA,
-          typename ElementB, typename LayoutBTag, int AlignmentB>
-using Gemm_ =
-    cutlass::gemm::device::GemmUniversalAdapter<
-                    GemmKernel<MmaTileShape, ClusterShape, PerSmTileShape_MNK,
-                               ArchTag,
-                               ElementA, LayoutATag, AlignmentA,
-                               ElementB, LayoutBTag, AlignmentB>>;
+template <typename MmaTileShape, typename ClusterShape,
+          typename PerSmTileShape_MNK, typename ArchTag, typename ElementA,
+          typename LayoutATag, int AlignmentA, typename ElementB,
+          typename LayoutBTag, int AlignmentB>
+using Gemm_ = cutlass::gemm::device::GemmUniversalAdapter<GemmKernel<
+    MmaTileShape, ClusterShape, PerSmTileShape_MNK, ArchTag, ElementA,
+    LayoutATag, AlignmentA, ElementB, LayoutBTag, AlignmentB>>;
 
 template <typename Gemm, typename ScaleType>
-typename Gemm::Arguments args_from_options(
-                                at::Tensor& D,
-                                at::Tensor const& A,
-                                at::Tensor const& B,
-                                at::Tensor const& A_sf,
-                                at::Tensor const& B_sf,
-                                float alpha,
-                                int M, int N, int K)
-{
-    using ElementA       = typename Gemm::ElementA;
-    using ElementB       = typename Gemm::ElementB;
-    using ElementD       = typename Gemm::ElementD;
-    using ElementSFA     = ScaleType;
-    using ElementSFB     = ScaleType;
-    using ElementCompute = float;
+typename Gemm::Arguments
+args_from_options(at::Tensor &D, at::Tensor const &A, at::Tensor const &B,
+                  at::Tensor const &A_sf, at::Tensor const &B_sf, float alpha,
+                  int M, int N, int K) {
+  using ElementA = typename Gemm::ElementA;
+  using ElementB = typename Gemm::ElementB;
+  using ElementD = typename Gemm::ElementD;
+  using ElementSFA = ScaleType;
+  using ElementSFB = ScaleType;
+  using ElementCompute = float;
 
-    using StrideA = typename Gemm::GemmKernel::StrideA;
-    using StrideB = typename Gemm::GemmKernel::StrideB;
-    using StrideC = typename Gemm::GemmKernel::StrideC;
-    using StrideD = typename Gemm::GemmKernel::StrideD;
+  using StrideA = typename Gemm::GemmKernel::StrideA;
+  using StrideB = typename Gemm::GemmKernel::StrideB;
+  using StrideC = typename Gemm::GemmKernel::StrideC;
+  using StrideD = typename Gemm::GemmKernel::StrideD;
 
-    using Sm1xxBlkScaledConfig =
-        typename Gemm::GemmKernel::CollectiveMainloop::Sm1xxBlkScaledConfig;
+  using Sm1xxBlkScaledConfig =
+      typename Gemm::GemmKernel::CollectiveMainloop::Sm1xxBlkScaledConfig;
 
-    auto stride_A = cutlass::make_cute_packed_stride(StrideA{}, {M, K, 1});
-    auto stride_B = cutlass::make_cute_packed_stride(StrideB{}, {N, K, 1});
-    auto stride_D = cutlass::make_cute_packed_stride(StrideD{}, {M, N, 1});
+  auto stride_A = cutlass::make_cute_packed_stride(StrideA{}, {M, K, 1});
+  auto stride_B = cutlass::make_cute_packed_stride(StrideB{}, {N, K, 1});
+  auto stride_D = cutlass::make_cute_packed_stride(StrideD{}, {M, N, 1});
 
-    auto layout_SFA = Sm1xxBlkScaledConfig::tile_atom_to_shape_SFA(
-        cute::make_shape(M, N, K, 1));
-    auto layout_SFB = Sm1xxBlkScaledConfig::tile_atom_to_shape_SFB(
-        cute::make_shape(M, N, K, 1));
+  auto layout_SFA = Sm1xxBlkScaledConfig::tile_atom_to_shape_SFA(
+      cute::make_shape(M, N, K, 1));
+  auto layout_SFB = Sm1xxBlkScaledConfig::tile_atom_to_shape_SFB(
+      cute::make_shape(M, N, K, 1));
 
-    typename Gemm::Arguments arguments{
-        cutlass::gemm::GemmUniversalMode::kGemm,
-        {M, N, K, 1},
-        {
-            static_cast<ElementA const*>(A.data_ptr()),      stride_A,
-            static_cast<ElementB const*>(B.data_ptr()),      stride_B,
-            static_cast<ElementSFA const*>(A_sf.data_ptr()), layout_SFA,
-            static_cast<ElementSFB const*>(B_sf.data_ptr()), layout_SFB},
-        {
-            {alpha, 0.f},
-            static_cast<ElementD const*>(D.data_ptr()), stride_D,
-            static_cast<ElementD*>(D.data_ptr()),       stride_D
-        }
-    };
+  typename Gemm::Arguments arguments{
+      cutlass::gemm::GemmUniversalMode::kGemm,
+      {M, N, K, 1},
+      {static_cast<ElementA const *>(A.data_ptr()), stride_A,
+       static_cast<ElementB const *>(B.data_ptr()), stride_B,
+       static_cast<ElementSFA const *>(A_sf.data_ptr()), layout_SFA,
+       static_cast<ElementSFB const *>(B_sf.data_ptr()), layout_SFB},
+      {{alpha, 0.f},
+       static_cast<ElementD const *>(D.data_ptr()),
+       stride_D,
+       static_cast<ElementD *>(D.data_ptr()),
+       stride_D}};
 
-    return arguments;
+  return arguments;
 }
 
 template <typename Gemm, typename ScaleType>
-void runGemm(at::Tensor& D,
-             at::Tensor const& A,
-             at::Tensor const& B,
-             at::Tensor const& A_sf,
-             at::Tensor const& B_sf,
-             float alpha,
-             int M, int N, int K,
-             torch::Device device)
-{
-    Gemm gemm;
+void runGemm(at::Tensor &D, at::Tensor const &A, at::Tensor const &B,
+             at::Tensor const &A_sf, at::Tensor const &B_sf, float alpha, int M,
+             int N, int K, torch::Device device) {
+  Gemm gemm;
 
-    auto arguments =
-        args_from_options<Gemm, ScaleType>(D, A, B, A_sf, B_sf, alpha, M, N, K);
+  auto arguments =
+      args_from_options<Gemm, ScaleType>(D, A, B, A_sf, B_sf, alpha, M, N, K);
 
-    size_t workspace_size = Gemm::get_workspace_size(arguments);
+  size_t workspace_size = Gemm::get_workspace_size(arguments);
 
-    cutlass::device_memory::allocation<uint8_t> workspace(workspace_size);
+  cutlass::device_memory::allocation<uint8_t> workspace(workspace_size);
 
-    const at::cuda::OptionalCUDAGuard device_guard(device_of(A));
-    cudaStream_t stream = at::cuda::getCurrentCUDAStream(device.index());
+  const at::cuda::OptionalCUDAGuard device_guard(device_of(A));
+  cudaStream_t stream = at::cuda::getCurrentCUDAStream(device.index());
 
-    CUTLASS_CHECK(gemm.can_implement(arguments));
+  CUTLASS_CHECK(gemm.can_implement(arguments));
 
-    CUTLASS_CHECK(gemm.initialize(arguments, workspace.get(), stream));
+  CUTLASS_CHECK(gemm.initialize(arguments, workspace.get(), stream));
 
-    CUTLASS_CHECK(gemm.run(arguments, workspace.get(), stream));
+  CUTLASS_CHECK(gemm.run(arguments, workspace.get(), stream));
 }
 
-void matmul_host_mxf4_bf16_tn(torch::Tensor& D,
-                              torch::Tensor const& A,
-                              torch::Tensor const& B,
-                              torch::Tensor const& A_sf,
-                              torch::Tensor const& B_sf,
-                              float alpha)
-{
-    auto const m = A.sizes()[0];
-    auto const n = B.sizes()[0];
-    auto const k = A.sizes()[1] * 2;
+void matmul_host_mxf4_bf16_tn(torch::Tensor &D, torch::Tensor const &A,
+                              torch::Tensor const &B, torch::Tensor const &A_sf,
+                              torch::Tensor const &B_sf, float alpha) {
+  auto const m = A.sizes()[0];
+  auto const n = B.sizes()[0];
+  auto const k = A.sizes()[1] * 2;
 
-    using ElementA   = cutlass::mx_float4_t<cutlass::float_e2m1_t>;
-    using LayoutATag = cutlass::layout::RowMajor;
-    static constexpr int AlignmentA = 128;
+  using ElementA = cutlass::mx_float4_t<cutlass::float_e2m1_t>;
+  using LayoutATag = cutlass::layout::RowMajor;
+  static constexpr int AlignmentA = 128;
 
-    using ElementB   = cutlass::mx_float4_t<cutlass::float_e2m1_t>;
-    using LayoutBTag = cutlass::layout::ColumnMajor;
-    static constexpr int AlignmentB = 128;
+  using ElementB = cutlass::mx_float4_t<cutlass::float_e2m1_t>;
+  using LayoutBTag = cutlass::layout::ColumnMajor;
+  static constexpr int AlignmentB = 128;
 
+  // Dynamically select architecture based on device capability
+  auto device_props = at::cuda::getCurrentDeviceProperties();
+  int major = device_props->major;
+  int minor = device_props->minor;
+
+  using ClusterShape = Shape<_1, _1, _1>;
+
+  if (major == 10 && minor == 0) {
+    // sm_100 (Blackwell B200) - M dimension must be 128 for sm_100
+    using ArchTag = cutlass::arch::Sm100;
+    using MmaTileShape = Shape<_128, _128, _128>;
+    using PerSmTileShape_MNK = Shape<_128, _128, _128>;
+
+    runGemm<
+        Gemm_<MmaTileShape, ClusterShape, PerSmTileShape_MNK, ArchTag, ElementA,
+              LayoutATag, AlignmentA, ElementB, LayoutBTag, AlignmentB>,
+        cutlass::float_ue8m0_t>(D, A, B, A_sf, B_sf, alpha, m, n, k,
+                                A.device());
+  } else if (major == 12 && minor == 0) {
+    // sm_120 (Blackwell B100)
     using ArchTag = cutlass::arch::Sm120;
-    using ClusterShape       = Shape<_1,_1,_1>;
-    if(m<512){
-        using MmaTileShape       = Shape<_128,_128,_128>;
-        using PerSmTileShape_MNK = Shape<_128,_128,_128>;
+    if (m < 512) {
+      using MmaTileShape = Shape<_128, _128, _128>;
+      using PerSmTileShape_MNK = Shape<_128, _128, _128>;
 
-        runGemm<Gemm_<MmaTileShape, ClusterShape, PerSmTileShape_MNK,
-                    ArchTag,
-                    ElementA, LayoutATag, AlignmentA,
-                    ElementB, LayoutBTag, AlignmentB>, cutlass::float_ue8m0_t
-                >(D, A, B, A_sf, B_sf, alpha, m, n, k, A.device());
+      runGemm<Gemm_<MmaTileShape, ClusterShape, PerSmTileShape_MNK, ArchTag,
+                    ElementA, LayoutATag, AlignmentA, ElementB, LayoutBTag,
+                    AlignmentB>,
+              cutlass::float_ue8m0_t>(D, A, B, A_sf, B_sf, alpha, m, n, k,
+                                      A.device());
     } else {
-        using MmaTileShape       = Shape<_256,_128,_128>;
-        using PerSmTileShape_MNK = Shape<_256,_128,_128>;
+      using MmaTileShape = Shape<_256, _128, _128>;
+      using PerSmTileShape_MNK = Shape<_256, _128, _128>;
 
-        runGemm<Gemm_<MmaTileShape, ClusterShape, PerSmTileShape_MNK,
-                    ArchTag,
-                    ElementA, LayoutATag, AlignmentA,
-                    ElementB, LayoutBTag, AlignmentB>, cutlass::float_ue8m0_t
-                >(D, A, B, A_sf, B_sf, alpha, m, n, k, A.device());
+      runGemm<Gemm_<MmaTileShape, ClusterShape, PerSmTileShape_MNK, ArchTag,
+                    ElementA, LayoutATag, AlignmentA, ElementB, LayoutBTag,
+                    AlignmentB>,
+              cutlass::float_ue8m0_t>(D, A, B, A_sf, B_sf, alpha, m, n, k,
+                                      A.device());
     }
+  } else {
+    TORCH_CHECK(false, "Unsupported CUDA architecture: sm_", major, minor,
+                ". This kernel requires sm_100 (Blackwell B200) or sm_120 "
+                "(Blackwell B100).");
+  }
 }
--- setup.py
+++ setup.py
@@ -14,71 +14,84 @@
 # limitations under the License.
 #
 
-from setuptools import setup, find_packages
-import torch.utils.cpp_extension as torch_cpp_ext
-from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 import os
-import pathlib, torch
+import pathlib
 import re
+
+from setuptools import find_packages, setup
 
 setup_dir = os.path.dirname(os.path.realpath(__file__))
 HERE = pathlib.Path(__file__).absolute().parent
-torch_version = torch.__version__
 
-def remove_unwanted_pytorch_nvcc_flags():
-    REMOVE_NVCC_FLAGS = [
-        '-D__CUDA_NO_HALF_OPERATORS__',
-        '-D__CUDA_NO_HALF_CONVERSIONS__',
-        '-D__CUDA_NO_BFLOAT16_CONVERSIONS__',
-        '-D__CUDA_NO_HALF2_OPERATORS__',
-    ]
-    for flag in REMOVE_NVCC_FLAGS:
-        try:
-            torch_cpp_ext.COMMON_NVCC_FLAGS.remove(flag)
-        except ValueError:
-            pass
-
-def get_cuda_arch_flags():
-    dev   = torch.cuda.current_device()
-    major, minor = torch.cuda.get_device_capability(dev)
-    arch_str = f"{major}{minor}a"
-    cc = major*10 + minor
-
-    flags = [
-        '-gencode', 'arch=compute_120a,code=sm_120a',
-        f'-DTARGET_CUDA_ARCH={cc}',
-        '--expt-relaxed-constexpr',
-        '--use_fast_math',
-        '-std=c++17',
-        '-O3',
-        '-DNDEBUG',
-        '-Xcompiler', '-funroll-loops',
-        '-Xcompiler', '-ffast-math',
-        '-Xcompiler', '-finline-functions',
-    ]
-    return flags
 
 def third_party_cmake():
-    import subprocess, sys, shutil
+    import shutil, subprocess, sys
 
-    cmake = shutil.which('cmake')
+    cmake = shutil.which("cmake")
     if cmake is None:
-            raise RuntimeError('Cannot find CMake executable.')
+        print("Warning: CMake executable not found. Skipping CMake configuration.")
+        print("If you encounter build issues, please install CMake and try again.")
+        return
 
     retcode = subprocess.call([cmake, HERE])
     if retcode != 0:
-        sys.stderr.write("Error: CMake configuration failed.\n")
-        sys.exit(1)
+        sys.stderr.write(
+            "Warning: CMake configuration failed, but continuing with build.\n"
+        )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
+    import torch
+    import torch.utils.cpp_extension as torch_cpp_ext
+    from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+
+    torch_version = torch.__version__
+
+    def remove_unwanted_pytorch_nvcc_flags():
+        REMOVE_NVCC_FLAGS = [
+            "-D__CUDA_NO_HALF_OPERATORS__",
+            "-D__CUDA_NO_HALF_CONVERSIONS__",
+            "-D__CUDA_NO_BFLOAT16_CONVERSIONS__",
+            "-D__CUDA_NO_HALF2_OPERATORS__",
+        ]
+        for flag in REMOVE_NVCC_FLAGS:
+            try:
+                torch_cpp_ext.COMMON_NVCC_FLAGS.remove(flag)
+            except ValueError:
+                pass
+
+    def get_cuda_arch_flags():
+        dev = torch.cuda.current_device()
+        major, minor = torch.cuda.get_device_capability(dev)
+        arch_str = f"{major}{minor}a"
+        cc = major * 10 + minor
+
+        flags = [
+            "-gencode",
+            f"arch=compute_{arch_str},code=sm_{arch_str}",
+            f"-DTARGET_CUDA_ARCH={cc}",
+            "--expt-relaxed-constexpr",
+            "--use_fast_math",
+            "-std=c++17",
+            "-O3",
+            "-DNDEBUG",
+            "-Xcompiler",
+            "-funroll-loops",
+            "-Xcompiler",
+            "-ffast-math",
+            "-Xcompiler",
+            "-finline-functions",
+        ]
+        return flags
+
     assert torch.cuda.is_available(), "CUDA is not available!"
     device = torch.cuda.current_device()
     print(f"Current device: {torch.cuda.get_device_name(device)}")
     print(f"Current CUDA capability: {torch.cuda.get_device_capability(device)}")
-    assert torch.cuda.get_device_capability(device)[0] >= 12, f"CUDA capability must be >= 12.0, yours is {torch.cuda.get_device_capability(device)}"
+    #  assert torch.cuda.get_device_capability(device)[0] >= 12, f"CUDA capability must be >= 12.0, yours is {torch.cuda.get_device_capability(device)}"
 
     print(f"PyTorch version: {torch_version}")
-    m = re.match(r'^(\d+)\.(\d+)', torch_version)
+    m = re.match(r"^(\d+)\.(\d+)", torch_version)
     if not m:
         raise RuntimeError(f"Cannot parse PyTorch version '{torch_version}'")
     major, minor = map(int, m.groups())
@@ -88,38 +101,36 @@ if __name__ == '__main__':
     third_party_cmake()
     remove_unwanted_pytorch_nvcc_flags()
     setup(
-        name='qutlass',
-        version='0.0.1',
-        author='Roberto L. Castro',
-        author_email='Roberto.LopezCastro@ist.ac.at',
-        description='CUTLASS-Powered Quantized BLAS for Deep Learning.',
+        name="qutlass",
+        version="0.0.1",
+        author="Roberto L. Castro",
+        author_email="Roberto.LopezCastro@ist.ac.at",
+        description="CUTLASS-Powered Quantized BLAS for Deep Learning.",
         packages=find_packages(),
         ext_modules=[
             CUDAExtension(
-                name='qutlass._CUDA',
+                name="qutlass._CUDA",
                 sources=[
-                    'qutlass/csrc/bindings.cpp',
-                    'qutlass/csrc/gemm.cu',
-                    'qutlass/csrc/gemm_ada.cu', #TODO (later): fuse into gemm.cu
-                    'qutlass/csrc/fused_quantize_mx.cu'
+                    "qutlass/csrc/bindings.cpp",
+                    "qutlass/csrc/gemm.cu",
+                    "qutlass/csrc/gemm_ada.cu",  # TODO (later): fuse into gemm.cu
+                    "qutlass/csrc/fused_quantize_mx.cu",
                 ],
                 include_dirs=[
-                    os.path.join(setup_dir, 'qutlass/csrc/include'),
-                    os.path.join(setup_dir, 'qutlass/csrc/include/cutlass_extentions'),
-                    os.path.join(setup_dir, 'third_party/cutlass/include'),
-                    os.path.join(setup_dir, 'third_party/cutlass/tools/util/include')
+                    os.path.join(setup_dir, "qutlass/csrc/include"),
+                    os.path.join(setup_dir, "qutlass/csrc/include/cutlass_extentions"),
+                    os.path.join(setup_dir, "third_party/cutlass/include"),
+                    os.path.join(setup_dir, "third_party/cutlass/tools/util/include"),
                 ],
                 extra_compile_args={
-                    'cxx': ["-std=c++17"],
-                    'nvcc': get_cuda_arch_flags(),
+                    "cxx": ["-std=c++17"],
+                    "nvcc": get_cuda_arch_flags(),
                 },
                 extra_link_args=[
-                '-lcudart',
-                '-lcuda',
-                ]
+                    "-lcudart",
+                    "-lcuda",
+                ],
             )
         ],
-        cmdclass={
-            'build_ext': BuildExtension
-        }
+        cmdclass={"build_ext": BuildExtension},
     )
